513c69abd2030b62293eaf26b3fd96f8b9a05340
zhangyong <zhangyong7120180@163.com>
2014-10-07 20:34:16 +0800

    android代码 天气

---
+++ src/com/mylife/tianqi/MainActivity.java
@@ -0,0 +1,23 @@
+package com.mylife.tianqi;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.app.Activity;
+import android.util.Log;
+import android.view.Menu;
+import android.widget.TextView;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+
+}
